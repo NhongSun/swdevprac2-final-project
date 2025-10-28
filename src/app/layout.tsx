84534db47921243a4a4cc7ee6ca610b715aba445
Patthapol Kittikun <patthapol.k@gmail.com>
2025-10-29 00:52:22 +0700

    add thai font and fix navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,15 @@ import { Toaster } from "@/components/ui/sonner";
 import { LocaleProvider } from "@/lib/locale-context";
 import { UserProvider } from "@/lib/user-context";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans_Thai } from "next/font/google";
 import type React from "react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ibmPlexSansThai = IBM_Plex_Sans_Thai({ 
+  subsets: ["latin", "thai"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Booth Booking System",
@@ -21,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${ibmPlexSansThai.className} min-h-dvh flex flex-col`}>
         <UserProvider>
           <LocaleProvider>
             <Navigation />
-            <main className="min-h-screen">{children}</main>
+            <main className="flex-1">{children}</main>
             <Toaster />
           </LocaleProvider>
         </UserProvider>
